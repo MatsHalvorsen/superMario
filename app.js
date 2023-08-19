ffeb94f50baf2ed88544db566479fedefc53e0eb
MatsHalvorsen <82614331+MatsHalvorsen@users.noreply.github.com>
2023-08-19 14:07:34 +0200

    let bowserHP was not defined

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-function startBattle(character, characterHp){
+function startBattle(character, characterHP){
     let selectedCharacterImg = document.getElementById('selectedCharacter');
     let resultText = document.getElementById('result');
 
     selectedCharacterImg.src = character + '.png';
 
-    let bowserHp = 300;
-    let battleResult = battle(character, characterHp);
+    let bowserHP = 300;
+    let battleResult = battle(characterHP, bowserHP);
 
     if (battleResult === 'win') {
         resultText.textContent = 'Du vant!';
@@ -17,7 +17,7 @@ function startBattle(character, characterHp){
 function battle(characterHP, bowserHP) {
     while (characterHP > 0 && bowserHP > 0) {
         characterHP -= Math.floor(Math.random() * 20) + 10;
-        bowserHP -= Math.floor(Math.random() * 30) + 10;
+        bowserHP -= Math.floor(Math.random() * 40) + 10;
     }
 
     if (characterHP <= 0) {
